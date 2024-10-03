4db7d758cb7587804f3eaf4b716925b63855120b
BadGenius22 <badgenius.crypto@gmail.com>
2024-10-03 13:35:51 +0700

    fix ci

--- next.config.js
+++ next.config.js
@@ -7,11 +7,19 @@ const nextConfig = {
     BASE_SEPOLIA_CHAIN_ID: String(process.env.BASE_SEPOLIA_CHAIN_ID || ''),
   },
   webpack: (config, { isServer }) => {
-    // Exclude contracts directory from being processed by webpack
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: [...(config.watchOptions.ignored || []), '**/contracts/**'],
-    };
+    // Ensure watchOptions and ignored exist
+    config.watchOptions = config.watchOptions || {};
+    config.watchOptions.ignored = config.watchOptions.ignored || [];
+
+    // Add contracts directory to ignored list
+    if (Array.isArray(config.watchOptions.ignored)) {
+      config.watchOptions.ignored.push('**/contracts/**');
+    } else if (typeof config.watchOptions.ignored === 'string') {
+      config.watchOptions.ignored = [config.watchOptions.ignored, '**/contracts/**'];
+    } else {
+      config.watchOptions.ignored = ['**/contracts/**'];
+    }
+
     return config;
   },
 };
