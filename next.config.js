1a75485a3bb43d89403c239e56faf51e24ecf3d5
BadGenius22 <badgenius.crypto@gmail.com>
2024-10-03 13:33:36 +0700

    fix ci

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     BASE_SEPOLIA_RPC_URL: process.env.BASE_SEPOLIA_RPC_URL || 'https://sepolia.base.org',
     BASE_SEPOLIA_CHAIN_ID: String(process.env.BASE_SEPOLIA_CHAIN_ID || ''),
   },
+  webpack: (config, { isServer }) => {
+    // Exclude contracts directory from being processed by webpack
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [...(config.watchOptions.ignored || []), '**/contracts/**'],
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
