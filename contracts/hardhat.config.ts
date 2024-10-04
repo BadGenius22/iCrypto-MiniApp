2e2216ada4daa538c783c9c15dcedce05a47cdfd
BadGenius22 <badgenius.crypto@gmail.com>
2024-10-04 12:29:34 +0700

    chore(config): hardhat

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,14 +1,15 @@
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-verify";
-import 'solidity-coverage';
-import 'hardhat-abi-exporter';
-import 'hardhat-contract-sizer';
-import '@openzeppelin/hardhat-upgrades';
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomicfoundation/hardhat-chai-matchers';
-import 'hardhat-tracer';
-import dotenv from 'dotenv';
+import "solidity-coverage";
+import "hardhat-abi-exporter";
+import "hardhat-contract-sizer";
+import "@openzeppelin/hardhat-upgrades";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "hardhat-tracer";
+import dotenv from "dotenv";
 import "@typechain/hardhat";
+import "./tasks";
 /* import * as tdly from '@tenderly/hardhat-tenderly'; */
 
 dotenv.config();
@@ -40,9 +41,9 @@ export const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.24',
+        version: "0.8.24",
         settings: {
-          evmVersion: 'cancun',
+          evmVersion: "cancun",
           optimizer: {
             enabled: true,
             runs: 200,
@@ -52,47 +53,35 @@ export const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    arbitrum: {
-      accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [DEPLOY_ACCOUNT_KEY!].filter(Boolean),
-      url: `https://arb-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
-    },
-    optimism: {
-      accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [DEPLOY_ACCOUNT_KEY!].filter(Boolean),
-      url: ` https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
-    },
     mainnet: {
       accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [DEPLOY_ACCOUNT_KEY!].filter(Boolean),
       url: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
     },
-    goerli: {
-      accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [DEPLOY_ACCOUNT_KEY!].filter(Boolean),
-      url: `https://arb-goerli.g.alchemy.com/v2/${ALCHEMY_KEY}`,
-    },
     base: {
       accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [DEPLOY_ACCOUNT_KEY!].filter(Boolean),
-      url: `https://mainnet.base.org`,
+      url: `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`,
     },
     baseSepolia: {
       accounts: MNEMONIC ? { mnemonic: MNEMONIC } : [DEPLOY_ACCOUNT_KEY!].filter(Boolean),
-      url: `https://sepolia.base.org`,
+      url: `https://base-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`,
     },
     tenderly: {
       url: `https://rpc.tenderly.co/fork/${TENDERLY_FORK_ID}`,
-      accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'],
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
     },
     hardhat: {
       initialBaseFeePerGas: 0,
     },
     local: {
       accounts: [
-        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-        '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+        "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
       ],
       url: `http://127.0.0.1:8545`,
     },
   },
   abiExporter: {
-    path: './abis',
+    path: "./abis",
     runOnCompile: true,
     clear: true,
     flat: false,
@@ -105,11 +94,11 @@ export const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      arbitrumOne: ARBISCAN_API_KEY || '', // Ensure non-null value
-      optimisticEthereum: OPTIMISM_ETHERSCAN_API_KEY || '', // Ensure non-null value
-      mainnet: ETHERSCAN_API_KEY || '', // Ensure non-null value
-      base: BASESCAN_API_KEY || '', // Added Base mainnet
-      baseSepolia: BASESCAN_API_KEY || '', // Added Base Sepolia
+      arbitrumOne: ARBISCAN_API_KEY || "", // Ensure non-null value
+      optimisticEthereum: OPTIMISM_ETHERSCAN_API_KEY || "", // Ensure non-null value
+      mainnet: ETHERSCAN_API_KEY || "", // Ensure non-null value
+      base: BASESCAN_API_KEY || "", // Added Base mainnet
+      baseSepolia: BASESCAN_API_KEY || "", // Added Base Sepolia
     },
   },
   mocha: {
@@ -123,13 +112,12 @@ export const config: HardhatUserConfig = {
   paths: {
     sources: "./src",
     cache: "./cache",
-    artifacts: "./artifacts"
+    artifacts: "./artifacts",
   },
   typechain: {
     outDir: "typechain-types",
     target: "ethers-v6",
   },
- 
 };
 
 export default config;
