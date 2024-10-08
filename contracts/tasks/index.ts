ecfde031ade4633066837f080330642d289ed016
BadGenius22 <badgenius.crypto@gmail.com>
2024-10-08 14:39:16 +0700

    feat(script): Add update merkle script

--- contracts/tasks/index.ts
+++ contracts/tasks/index.ts
@@ -3,6 +3,7 @@ export * from "./rewardDistributor/deploy-reward-distributor";
 export * from "./rewardDistributor/deploy-reward-distributor-implementation";
 export * from "./rewardDistributor/deposit-rewards";
 export * from "./rewardDistributor/whitelist-token";
+export * from "./rewardDistributor/update-merkle-root";
 
 // tokens
 export * from "./tokens/deploy-icrypto-token";
